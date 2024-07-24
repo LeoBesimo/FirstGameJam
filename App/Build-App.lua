bf7ec3da5c4b030790c4a8c75506c5cd9918dccb
Leo Besimo <besimol515@gmail.com>
2024-07-24 23:34:13 +0200

    initial setup, linking sfml

--- App/Build-App.lua
+++ App/Build-App.lua
@@ -5,19 +5,41 @@ project "App"
    targetdir "Binaries/%{cfg.buildcfg}"
    staticruntime "off"
 
-   files { "Source/**.h", "Source/**.cpp" }
+   files { "Source/**.h", "Source/**.cpp", "Source/**.hpp" }
 
    includedirs
    {
       "Source",
 
 	  -- Include Core
-	  "../Core/Source"
+	  "../Core/Source",
+      "../Vendor/Binaries/SFML-2.6.1/include"
+   }
+
+   libdirs
+   {
+        "../Vendor/Binaries/SFML-2.6.1/lib"
    }
 
    links
    {
-      "Core"
+      "Core",
+      "winmm.lib",
+      "ws2_32.lib",
+      "openal32.lib",
+      "flac.lib",
+      "vorbisenc.lib",
+      "vorbisfile.lib",
+      "vorbis.lib",
+      "ogg.lib",
+      "gdi32.lib",
+      "opengl32.lib",
+      "freetype.lib"
+   }
+
+   defines 
+   {
+    "SFML_STATIC"
    }
 
    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
@@ -29,17 +51,41 @@ project "App"
 
    filter "configurations:Debug"
        defines { "DEBUG" }
+       links 
+       { 
+        "sfml-system-s-d.lib",
+        "sfml-network-s-d.lib",
+        "sfml-audio-s-d.lib",
+        "sfml-window-s-d.lib",
+        "sfml-graphics-s-d.lib"
+       }
        runtime "Debug"
        symbols "On"
 
    filter "configurations:Release"
        defines { "RELEASE" }
+       links 
+       { 
+        "sfml-system-s.lib",
+        "sfml-network-s.lib",
+        "sfml-audio-s.lib",
+        "sfml-window-s.lib",
+        "sfml-graphics-s.lib"
+       }
        runtime "Release"
        optimize "On"
        symbols "On"
 
    filter "configurations:Dist"
        defines { "DIST" }
+       links 
+       { 
+        "sfml-system-s.lib",
+        "sfml-network-s.lib",
+        "sfml-audio-s.lib",
+        "sfml-window-s.lib",
+        "sfml-graphics-s.lib"
+       }
        runtime "Release"
        optimize "On"
        symbols "Off"
--- Core/Build-Core.lua
+++ Core/Build-Core.lua
@@ -5,11 +5,37 @@ project "Core"
    targetdir "Binaries/%{cfg.buildcfg}"
    staticruntime "off"
 
-   files { "Source/**.h", "Source/**.cpp" }
+   files { "Source/**.h", "Source/**.cpp", "Source/**.hpp" }
 
    includedirs
    {
-      "Source"
+      "Source",
+      "../Vendor/Binaries/SFML-2.6.1/include"
+   }
+
+   libdirs
+   {
+        "../Vendor/Binaries/SFML-2.6.1/lib"
+   }
+
+   links
+   {
+      "winmm.lib",
+      "ws2_32.lib",
+      "openal32.lib",
+      "flac.lib",
+      "vorbisenc.lib",
+      "vorbisfile.lib",
+      "vorbis.lib",
+      "ogg.lib",
+      "gdi32.lib",
+      "opengl32.lib",
+      "freetype.lib"
+   }
+
+   defines
+   {
+    "SFML_STATIC"
    }
 
    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
@@ -21,17 +47,41 @@ project "Core"
 
    filter "configurations:Debug"
        defines { "DEBUG" }
+       links 
+       { 
+        "sfml-system-s-d.lib",
+        "sfml-network-s-d.lib",
+        "sfml-audio-s-d.lib",
+        "sfml-window-s-d.lib",
+        "sfml-graphics-s-d.lib"
+       }
        runtime "Debug"
        symbols "On"
 
    filter "configurations:Release"
        defines { "RELEASE" }
+       links 
+       { 
+        "sfml-system-s.lib",
+        "sfml-network-s.lib",
+        "sfml-audio-s.lib",
+        "sfml-window-s.lib",
+        "sfml-graphics-s.lib"
+       }
        runtime "Release"
        optimize "On"
        symbols "On"
 
    filter "configurations:Dist"
        defines { "DIST" }
+       links 
+       { 
+        "sfml-system-s.lib",
+        "sfml-network-s.lib",
+        "sfml-audio-s.lib",
+        "sfml-window-s.lib",
+        "sfml-graphics-s.lib"
+       }
        runtime "Release"
        optimize "On"
        symbols "Off"
