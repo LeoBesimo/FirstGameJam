ea7d2daa8db488475247a1947f6e45b9a1db2571
BaLion29 <bastien.aeby@familyaeby.ch>
2024-07-27 00:48:16 +0200

    FillColor

--- Core/Source/Core/Renderer/Window.cpp
+++ Core/Source/Core/Renderer/Window.cpp
@@ -27,10 +27,11 @@ std::vector<sf::Event> Core::Window::getEvents()
 	return events;
 }
 
-void Core::Window::rect(float positionX, float positionY, float width, float height)
+void Core::Window::rect(float x, float y, float width, float height)
 {
-	m_RectangleShape.setPosition(sf::Vector2f(positionX, positionY));
+	m_RectangleShape.setPosition(sf::Vector2f(x, y));
 	m_RectangleShape.setSize(sf::Vector2f(width, height));
+	m_RectangleShape.setFillColor(m_FillColor);
 
 	m_Window.draw(m_RectangleShape);
 }
@@ -43,6 +44,21 @@ void Core::Window::line(float x1, float y1, float x2, float y2)
 	};
 
 	m_Window.draw(line, 2, sf::Lines);
+}
+
+void Core::Window::circle(float x, float y, float radius)
+{
+	m_CircleShape.setPosition(sf::Vector2f(x, y));
+	m_CircleShape.setRadius(radius);
+	m_CircleShape.setFillColor(m_FillColor);
+
+	m_Window.draw(m_CircleShape);
+}
+
+void Core::Window::fill(int r, int g, int b, int alpha)
+{
+	sf::Color fillColor(r, g, b, alpha);
+	m_FillColor = fillColor;
 }
 
 void Core::Window::clear(int r, int g, int b, int alpha)
--- Core/Source/Core/Renderer/Window.hpp
+++ Core/Source/Core/Renderer/Window.hpp
@@ -18,8 +18,11 @@ namespace Core
 
 		void rect(float x, float y, float width, float height);
 		void line(float x1, float y1, float x2, float y2);
+		void circle(float x, float y, float radius);
 
-		void clear(int r, int g, int b, int alpha);
+		void fill(int r = 0, int g = 0, int b = 0, int alpha = 1);
+
+		void clear(int r = 255, int g = 255, int b = 255, int alpha = 1);
 		void display();
 		void close();
 
